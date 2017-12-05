f236d7a396b413cc6fbbce8be4c6ecb44eed4521
thothi <23506212+thothi@users.noreply.github.com>
2017-12-05 12:59:57 +0100

    Added random image feature -bild

--- lard.py
+++ lard.py
@@ -4,6 +4,7 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
 import random
+import os
 
 client = Bot(description="", command_prefix="-", pm_help = True)
 
@@ -17,14 +18,18 @@ async def on_ready():
 	print('Use this link to invite {}:'.format(client.user.name))
 	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('-bild'):
+        bild='lardpics/'
+        randombild = random.choice(os.listdir(bild))
+        bild += randombild
+        await client.send_file(message.channel, bild)
 
-@client.command()
-async def zitat(*args):
-    with open('lard.txt') as f:
-        quotes = f.readlines()
-        quotes = [x.strip() for x in quotes]
+    if message.content.startswith('-zitat'):
+        with open('lard.txt') as f:
+            quotes = f.readlines()
+            quotes = [x.strip() for x in quotes]
+        await client.send_message(message.channel, random.choice(quotes))
 
-    await client.say(random.choice(quotes))
-    await asyncio.sleep(3)
-
-client.run('Mzg3MTkyNzI5NzU2NzYyMTE2.DQcZEA.PjRlDuCJM1YyVBNy5fWkgthVLJc')
+client.run('Mzg3MjI4MDA5Nzc5ODIyNTky.DQbxmg.I3fSZb6GhxpLU85SxHhShd-ihwg')
