504b3b2fbd3df5c562cd200bdc49510b4916a277
Ubuntu <ubuntu@ip-172-31-44-19.eu-central-1.compute.internal>
2017-12-06 23:07:44 +0000

    Added automatic update script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python3 lard.py &
+while true
+do
+     git pull origin master > log.txt
+     sleep 60s
+     if ! grep -q 'Already up-to-date.' log.txt; then
+          pkill -f lard.py
+          sleep 10s
+          python3 lard.py &
+     fi     
+     sleep 1h 
+done
